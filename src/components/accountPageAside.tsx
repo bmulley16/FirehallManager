666d6e670a25cbb1adae063b28dd0490ae64ae50
bmulley16 <bmulley9816@gmail.com>
2023-08-21 18:41:41 -0230

    added the accountpage aside component and constructin the layout of the profile dashboard

---
+++ src/components/accountPageAside.tsx
@@ -0,0 +1,22 @@
+export function ProfileAside() {
+  return (
+    <aside>
+      <h1>Emergico</h1>
+
+      <h4>Navigation:</h4>
+      {/* may have to come fool with this layout after to make it look nicer */}
+      <div>
+        <ul>
+          <li>Administration</li>
+          <li>Equipment</li>
+          <li>Overtime</li>
+          <li>Training</li>
+          <li>Contacts</li>
+          <li>Mess Managment</li>
+
+          <li></li>
+        </ul>
+      </div>
+    </aside>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App.tsx";
 import "./index.css";
-
+import { Profile } from "./components/pages/profile.tsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import LoginPage from "./components/pages/loginpage.tsx";
 import AccountLandingPage from "./components/pages/accountLandingPage.tsx";
@@ -22,7 +22,10 @@ const router = createBrowserRouter([
     element: <AccountLandingPage />,
   },
 
-  {},
+  {
+    path: "/profile",
+    element: <Profile />,
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
