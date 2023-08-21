939ae1ff6fee842c68e3d325a8ca0424a9864093
bmulley16 <bmulley9816@gmail.com>
2023-08-21 19:38:55 -0230

    background image foxed on account landing page

--- src/components/pages/accountLandingPage.tsx
+++ src/components/pages/accountLandingPage.tsx
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom";
 const AccountLandingPage = () => {
   return (
     <div
-      className="flex flex-col items-center justify-center min-h-screen bg-cover"
-      style={{
-        backgroundImage:
-          "url('/firehallmanager/images/fire-trucks-down-center-of-road(accountlandingpage).jpg')",
-      }}
+      className="flex flex-col items-center justify-center min-h-screen bg-cover bg-[url('src\\assets\\fire-trucks-down-center-of-road.jpg')] "
+      // style={{
+      //   backgroundImage:
+      //     "url('/firehallmanager/images/fire-trucks-down-center-of-road(accountlandingpage).jpg')",
+      // }}
     >
       <div className="max-w-md w-full bg-white shadow p-6 rounded-lg bg-opacity-75">
         <h1 className="text-3xl font-semibold mb-4 text-gray-800">
