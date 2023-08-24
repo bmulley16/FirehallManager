e5974d56a72bba9fec51ae77ea38e53d859473a7
bmulley16 <bmulley9816@gmail.com>
2023-08-24 20:53:41 -0230

    have the dashboard grid layed out

--- src/components/pages/profile.tsx
+++ src/components/pages/profile.tsx
@@ -2,7 +2,7 @@ import { ProfileAside } from "../accountPageAside";
 import { MainDashboard } from "../profileMainDashboard";
 export function Profile() {
   return (
-    <div className=" flex ">
+    <div className=" flex w-screen h-screen ">
       <ProfileAside></ProfileAside>
       <MainDashboard></MainDashboard>
     </div>
--- src/components/profileMainDashboard.tsx
+++ src/components/profileMainDashboard.tsx
@@ -1,3 +1,5 @@
 export function MainDashboard() {
-  return <div> Hello world</div>;
+  return (
+    <div className="flex-auto grid grid-cols-2 grid-rows-3 gap-x-[30px] gap-y-[10px] "></div>
+  );
 }
