ac92aba94bc720c00f23683b3cbd1e1b7a0faeb3
bmulley16 <bmulley9816@gmail.com>
2023-08-28 22:30:54 -0230

    Got the flex grid laid out correctly on the profile dashboard

--- src/components/profileMainDashboard.tsx
+++ src/components/profileMainDashboard.tsx
@@ -1,5 +1,12 @@
 export function MainDashboard() {
   return (
-    <div className="flex-auto grid grid-cols-2 grid-rows-3 gap-x-[30px] gap-y-[10px] "></div>
+    <div className="flex-auto grid grid-cols-3 grid-rows-3 gap-x-[5px]  gap-y-[10px] ml-7 ">
+      <div className=" bg-black col-span-2 rounded-2xl"></div>
+      <div className=" bg-black row-span-2 w-2/3 mx-auto rounded-2xl "></div>
+      <div className=" bg-red-500 rounded-2xl"></div>
+      <div className=" bg-red-500 rounded-2xl"></div>
+      <div className=" bg-red-500 col-span-2 rounded-2xl"></div>
+      <div className=" bg-red-500 w-2/3 mx-auto rounded-2xl"></div>
+    </div>
   );
 }
