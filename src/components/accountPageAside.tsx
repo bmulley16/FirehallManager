1d34ded5fc9094c8cc57c585b0c1c17240898d93
bmulley16 <bmulley9816@gmail.com>
2023-08-29 18:32:43 -0230

    added the logo and edited the aside bar some more

--- src/components/accountPageAside.tsx
+++ src/components/accountPageAside.tsx
@@ -1,20 +1,32 @@
 export function ProfileAside() {
   return (
-    <aside className=" w-60 bg-white shadow rounded-lg bg-opacity-75  min-h-screen p-0 ">
-      <h1>Emergico</h1>
-
-      <h4>Navigation:</h4>
-      {/* may have to come fool with this layout after to make it look nicer */}
+    <aside className=" w-60 shadow   min-h-screen p-0 text-center flex flex-col  content-center bg-gray-900 space-x-4 text-white ">
       <div>
-        <ul>
+        <img
+          className="h-32 w-32 rounded-full mx-auto mt-5 "
+          src="src\assets\Emergico-Logo.jpg"
+          alt="Comany Logo"
+        />
+        <h1 className=" mt-10">
+          <span className="text-4xl underline decoration-inherit ">
+            Welcome
+          </span>
+          <br />
+          <br />
+          <br />
+          <span className=" text-2xl">Bryce Mulley</span>
+        </h1>
+      </div>
+
+      <div className="my-auto mx-auto">
+        <ul className=" h-full w-full flex flex-col text-left justify-evenly">
           <li>Administration</li>
           <li>Equipment</li>
+          <li>Inventories</li>
           <li>Overtime</li>
           <li>Training</li>
-          <li>Contacts</li>
           <li>Mess Managment</li>
-
-          <li></li>
+          <li>Contacts</li>
         </ul>
       </div>
     </aside>
