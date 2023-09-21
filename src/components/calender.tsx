1fbb760a2f664106556b5d8283e0de0072feda6d
bmulley16 <bmulley9816@gmail.com>
2023-09-20 22:35:18 -0230

    added the logic for calender rendering dates using luxon

--- src/components/calender.tsx
+++ src/components/calender.tsx
@@ -1,52 +1,29 @@
+import { DateTime } from "luxon";
 import Cell from "./cell";
 
 const daysofWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", " Fri", "Sat"];
 
+const currentTime = DateTime.local();
+
+const currentMonth = currentTime.month;
+const daysInMonth = currentTime.daysInMonth;
+
 export function Calender() {
+  console.log(currentMonth);
   return (
     <div className=" w-[400] border border-t border-l">
       <div className="grid grid-cols-7 items-center justify-center text-center">
         <Cell>{"<<"}</Cell>
         <Cell>{"<"}</Cell>
-        <Cell className="col-span-3">{"August 2022"}</Cell>
+        <Cell className="col-span-3">{"aa"}</Cell>
         <Cell>{">"}</Cell>
         <Cell>{">>"}</Cell>
 
         {daysofWeek.map((day) => (
-          <Cell className=" text-sm font-bold">{day}</Cell>
+          <Cell key={day} className=" text-sm font-bold">
+            {day}
+          </Cell>
         ))}
-
-        <Cell>1</Cell>
-        <Cell>2</Cell>
-        <Cell>3</Cell>
-        <Cell>4</Cell>
-        <Cell>5</Cell>
-        <Cell>6</Cell>
-        <Cell>7</Cell>
-        <Cell>8</Cell>
-        <Cell>9</Cell>
-        <Cell>10</Cell>
-        <Cell>11</Cell>
-        <Cell>12</Cell>
-        <Cell>13</Cell>
-        <Cell>14</Cell>
-        <Cell>15</Cell>
-        <Cell>16</Cell>
-        <Cell>17</Cell>
-        <Cell>18</Cell>
-        <Cell>19</Cell>
-        <Cell>20</Cell>
-        <Cell>21</Cell>
-        <Cell>22</Cell>
-        <Cell>23</Cell>
-        <Cell>24</Cell>
-        <Cell>25</Cell>
-        <Cell>26</Cell>
-        <Cell>27</Cell>
-        <Cell>28</Cell>
-        <Cell>29</Cell>
-        <Cell>30</Cell>
-        <Cell>31</Cell>
       </div>
     </div>
   );
