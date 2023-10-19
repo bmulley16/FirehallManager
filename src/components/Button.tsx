c3f31a1051bda0b08d9fe55f72a383d4bf2fcaec
bmulley16 <bmulley9816@gmail.com>
2023-10-19 21:06:34 -0230

    fixed button pages

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,8 +1,12 @@
+import { Link } from "react-router-dom";
+
 function Button() {
   return (
-    <button className="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-full mt-4 focus:outline-none focus:ring focus:ring-blue-200">
-      Get Started
-    </button>
+    <Link to="/login">
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-full mt-4 focus:outline-none focus:ring focus:ring-blue-200">
+        Get Started
+      </button>
+    </Link>
   );
 }
 
