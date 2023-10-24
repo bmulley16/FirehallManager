065696c07f987052f034b0c3fad08e9974124a21
bmulley16 <bmulley9816@gmail.com>
2023-10-23 22:49:41 -0230

    began to implement the grid for the training page

--- src/pages/trainingPage.tsx
+++ src/pages/trainingPage.tsx
@@ -2,10 +2,14 @@ import { ProfileAside } from "../components/accountPageAside";
 
 export function TrainingPage() {
   return (
-    <div className="flex-1 h-screen w-screen">
+    <div className="flex-1 flex h-screen w-screen">
       <ProfileAside></ProfileAside>
 
-      <div className=" h-full w-full "></div>
+      <div className=" h-full w-full grid grid-cols-2  ">
+        <div className=" col-span-2">
+          <h1 className="text-center ">Today's Training</h1>
+        </div>
+      </div>
     </div>
   );
 }
