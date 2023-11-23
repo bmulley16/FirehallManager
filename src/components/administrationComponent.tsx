0e12bdf0cab6c4859aaaeab8e9b6a1320a68d712
bmulley16 <bmulley9816@gmail.com>
2023-11-23 00:21:29 -0330

    changed the administration component

--- src/components/administrationComponent.tsx
+++ src/components/administrationComponent.tsx
@@ -4,9 +4,9 @@ import { AdministrationSelector } from "../types";
 export function Administration(props: AdministrationSelector) {
   return (
     <div className="bg-white rounded-lg shadow-md  hover:bg-green-500 w-2/3 ">
-      <h1 className=" hover:font-bold m-4 text-xl font-semibold text-center">
+      <button className=" hover:font-bold text-xl p-4 font-semibold text-center w-full h-full">
         {props.title}
-      </h1>
+      </button>
     </div>
   );
 }
