bd52ee01df00de3de7a23cd7a5689decf7bc6c3d
bmulley16 <bmulley9816@gmail.com>
2023-11-24 23:09:42 -0330

    added some things to the station component, but am working on the dynamics of the page

---
+++ src/components/stattionGearComponent.tsx
@@ -0,0 +1,11 @@
+export interface Equipment {}
+
+export function StationGear() {
+  const enteredTimes = {};
+  return (
+    <div>
+      <h1>Station Gear</h1>
+      <div>Your Next Issue will be </div>
+    </div>
+  );
+}
