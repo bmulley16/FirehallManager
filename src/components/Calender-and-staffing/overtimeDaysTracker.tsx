d007536e2cd774a167f8cffdad548c1fbe5139c8
bmulley16 <bmulley9816@gmail.com>
2024-01-27 21:45:05 -0330

    added the comments to the overtimeDaysTracker

--- src/components/Calender-and-staffing/overtimeDaysTracker.tsx
+++ src/components/Calender-and-staffing/overtimeDaysTracker.tsx
@@ -1,9 +1,19 @@
 import { DateTime } from "luxon";
-
-export const shiftTracker = () => {
-  const SHIFT_LENGTH = 96;
+const currentDateTime = DateTime.now(); 
+const SHIFT_LENGTH = 96;
   const PLATOONS = ["A", "B", "C", "D"];
-  const currentDateTime = DateTime.now();
+/**
+
+ * 2. Return an object that contains the shift info
+ *    - Current platoon
+ *    - Elapsed hours within cycle
+ *    - Next shift start time
+ * 3. Move any constant variables to the top of the file
+ */
+
+export const shiftTracker = (currentDateTime:any) => {
+  
+ 
   const initialDateTime = DateTime.local(2024, 1, 0o3, 8, 0o0);
 
   const elapsedHours = currentDateTime.diff(initialDateTime, ["hour"]).hours;
@@ -20,4 +30,16 @@ export const shiftTracker = () => {
   const nextShiftStartTime = currentDateTime.plus({
     hours: SHIFT_LENGTH - (elapsedHours % SHIFT_LENGTH),
   });
+
+  const previousShiftStartTime = currentDateTime.minus({
+    hours: elapsedHours % SHIFT_LENGTH,
+  });
+
+
+  return {
+    currentPlatoon,
+    elapsedWithinCycle,
+    nextShiftStartTime,
+    previousShiftStartTime
+  };
 };
