30fa0b512de073562057071d37b58570d58c771a
bmulley16 <bmulley9816@gmail.com>
2024-03-05 17:48:53 -0330

    added some more logic for rendering every user for overtime and day/night shift ect.

--- src/components/userInfoCardOvertimeCallBack.tsx
+++ src/components/userInfoCardOvertimeCallBack.tsx
@@ -1,21 +1,24 @@
 import React from 'react';
 import { OvertimeLogic } from '../utils/api';
+import { User } from '../types';
 
-export const UserInfoCard = () => {
-  const overtimeUsersOrder = OvertimeLogic();
-console.log(overtimeUsersOrder)
+export const UserInfoCard = (props: any) => {
+ 
+  const { dayshift, nightShift, emergencyCallback, fourHourCallBack } = props;
+
+  const users = dayshift || nightShift || emergencyCallback || fourHourCallBack || [];
+
   return (
     <div>
-      {overtimeUsersOrder.map((user, index) => (
+      {users.map((user: User, index: number) => (
         <div key={index} className="mb-4 p-4 bg-gray-200 rounded">
           <h2 className="text-lg font-semibold">{user.firstName} {user.lastName}</h2>
           <p className="text-gray-600">{user.phone}</p>
-
         </div>
       ))}
     </div>
-   
   );
 };
+
 
 
--- src/pages/overtimeOrderPage.tsx
+++ src/pages/overtimeOrderPage.tsx
@@ -1,13 +1,25 @@
 
 import React from 'react';
 import { UserInfoCard } from '../components/userInfoCardOvertimeCallBack';
+import { OvertimeLogic } from '../utils/api';
+
 
 export const OvertimeOrderDisplay = () => {
+  const overtimeUsersOrder = OvertimeLogic();
+  const nightShift = overtimeUsersOrder[0]
+  const dayshift = overtimeUsersOrder[1]
+  const emergencyCallback =  overtimeUsersOrder[2]
+  const fourHourCallBack = overtimeUsersOrder[3]
+  
   return (
     <div className="min-h-screen bg-gray-100 flex items-center justify-center">
       <div className="bg-white p-8 rounded shadow-md w-full max-w-lg">
       <h1 className="text-3xl font-bold mb-6 text-center">Overtime Callback Order</h1>
-      <UserInfoCard></UserInfoCard>
+      <UserInfoCard dayshift={dayshift} ></UserInfoCard>
+      <UserInfoCard nightShift={nightShift}></UserInfoCard>
+      <UserInfoCard emergencyCallback={emergencyCallback}></UserInfoCard>
+      <UserInfoCard fourHourCallBack={fourHourCallBack}></UserInfoCard>
+
       </div>
     </div>
   );
